18ba0d9dd89caf651d2660cc4c022efed5bd297d
Nguyễn Thanh Ý <85022353+thanhy123zzz@users.noreply.github.com>
2022-12-13 00:19:10 +0700

    update don vi tinh
    update don vi tinh

--- VatTuYTeDanhMuc/Controllers/DonViTinhController.cs
+++ VatTuYTeDanhMuc/Controllers/DonViTinhController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using VatTuYTeDanhMuc.Models.Entities;
 
 namespace VatTuYTeDanhMuc.Controllers
 {
@@ -8,6 +10,65 @@ namespace VatTuYTeDanhMuc.Controllers
         {
             ViewData["Title"] = "Danh mục đơn vị tính";
             return View("TableDonViTinh");
+        }
+
+        //Trả về view thêm đơn vị tính
+        public IActionResult ViewInsertDVT()
+        {
+            ViewData["Title"] = "Thêm đơn vị tính";
+            return View();
+        }
+        //Trả về view sửa đơn vị tính
+        [Route("/DonViTinh/ViewUpdateDVT/{id}")]
+        public IActionResult ViewUpdateDVT(int id)
+        {
+            ViewData["Title"] = "Sửa đơn vị tính";
+            webContext context = new webContext();
+            Dvt dvt = context.Dvt.Find(id);
+            return View(dvt);
+        }
+
+        //insert đơn vị tính
+        [HttpPost]
+        public IActionResult insertDonViTinh(Dvt dvt)
+        {
+            webContext context = new webContext();
+            dvt.Nvtao =3;
+            dvt.Active = true;
+            dvt.NgayTao = DateTime.Now;
+            context.Dvt.Add(dvt);
+            context.SaveChanges();
+            return RedirectToAction("Table");
+        }
+
+        //update đơn vị tính
+        [HttpPost]
+        public IActionResult updateDVt(Dvt dvt)
+        {
+            webContext context = new webContext();
+            Dvt dv = context.Dvt.Find(dvt.Id);
+
+            dv.Nvsua = 3;
+            dv.NgaySua = DateTime.Now;
+            dv.TenDvt = dvt.TenDvt;
+            dv.MaDvt = dvt.MaDvt;
+
+            context.Dvt.Update(dv);
+            context.SaveChanges();
+            return RedirectToAction("Table");
+        }
+
+        //Xoá đơn vị tính
+        [Route("/DonViTinh/xoa/{id}")]
+        public IActionResult deleteDonViTinh(int id)
+        {
+            webContext context = new webContext();
+            Dvt dvt = context.Dvt.Find(id);
+            dvt.Active=false;
+
+            context.Dvt.Update(dvt);
+            context.SaveChanges();
+            return RedirectToAction("Table");
         }
     }
 }
