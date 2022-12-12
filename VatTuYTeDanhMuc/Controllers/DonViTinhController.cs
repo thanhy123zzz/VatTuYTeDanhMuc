59c27bdefb8c4447718b2dd8a7d4bcbc698ba6bb
Nguyễn Thanh Ý <85022353+thanhy123zzz@users.noreply.github.com>
2022-12-12 21:49:32 +0700

    update

---
+++ VatTuYTeDanhMuc/Controllers/DonViTinhController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VatTuYTeDanhMuc.Controllers
+{
+    public class DonViTinhController : Controller
+    {
+        public IActionResult Table()
+        {
+            return View("TableDonViTinh");
+        }
+    }
+}
---
+++ VatTuYTeDanhMuc/Controllers/NuocSanXuat.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VatTuYTeDanhMuc.Controllers
+{
+    public class NuocSanXuat : Controller
+    {
+        public IActionResult Table()
+        {
+            return View("TableNuocSX");
+        }
+    }
+}
