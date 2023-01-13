767e02133a7d3a03c89efa5437cd42f63f857f3e
Bao Yen <nneyel104@gmail.com>
2023-01-13 13:59:01 +0700

    login - test pq dvt

--- VatTuYTeDanhMuc/Controllers/DonViTinhController.cs
+++ VatTuYTeDanhMuc/Controllers/DonViTinhController.cs
@@ -6,99 +6,102 @@ using VatTuYTeDanhMuc.Models.Entities;
 
 namespace VatTuYTeDanhMuc.Controllers
 {
-    public class DonViTinhController : Controller
+  public class DonViTinhController : Controller
+  {
+    public IActionResult Table()
     {
-        public IActionResult Table()
-        {
-            ViewData["Title"] = "Danh mục đơn vị tính";
-            return View("TableDonViTinh");
-        }
-
-        //Trả về view thêm đơn vị tính
-        public IActionResult ViewInsertDVT()
-        {
-            ViewData["Title"] = "Thêm đơn vị tính";
-            return View();
-        }
-        [HttpPost("/loadTableDVT")]
-        public IActionResult loadTableDVT(bool active)
-        {
-            webContext context = new webContext();
-            if (active) {
-                ViewBag.DVT = context.Dvt.Where(x => x.Active == true).ToList();
-            }
-            else
-            {
-                ViewBag.DVT = context.Dvt.ToList();
-            }
-            return PartialView();
-        }
-
-        //Trả về view sửa đơn vị tính
-        [Route("/DonViTinh/ViewUpdateDVT/{id}")]
-        public IActionResult ViewUpdateDVT(int id)
-        {
-            ViewData["Title"] = "Sửa đơn vị tính";
-            webContext context = new webContext();
-            Dvt dvt = context.Dvt.Find(id);
-            return View(dvt);
-        }
-
-        //insert đơn vị tính
-        [HttpPost]
-        public IActionResult insertDonViTinh(Dvt dvt)
-        {
-            webContext context = new webContext();
-            dvt.Nvtao =3;
-            dvt.Active = true;
-            dvt.NgayTao = DateTime.Now;
-            context.Dvt.Add(dvt);
-            context.SaveChanges();
-            TempData["ThongBao"] = "Thêm thành công!";
-            return RedirectToAction("Table");
-        }
-
-        //update đơn vị tính
-        [HttpPost]
-        public IActionResult updateDVt(Dvt dvt)
-        {
-            webContext context = new webContext();
-            Dvt dv = context.Dvt.Find(dvt.Id);
-
-            dv.Nvsua = 3;
-            dv.NgaySua = DateTime.Now;
-            dv.TenDvt = dvt.TenDvt;
-            dv.MaDvt = dvt.MaDvt;
-
-            context.Dvt.Update(dv);
-            context.SaveChanges();
-            TempData["ThongBao"] = "Sửa thành công!";
-            return RedirectToAction("Table");
-        }
-
-        //Xoá đơn vị tính
-        [Route("/DonViTinh/xoa/{id}")]
-        public IActionResult deleteDonViTinh(int id)
-        {
-            webContext context = new webContext();
-            Dvt dvt = context.Dvt.Find(id);
-            dvt.Active=false;
-
-            context.Dvt.Update(dvt);
-            context.SaveChanges();
-            return RedirectToAction("Table");
-        }
-        [Route("/DonViTinh/khoiphuc/{id}")]
-        public IActionResult khoiphucDVT(int id)
-        {
-            webContext context = new webContext();
-            Dvt dvt = context.Dvt.Find(id);
-            dvt.Active = true;
-
-            context.Dvt.Update(dvt);
-            context.SaveChanges();
-            TempData["ThongBao"] = "Khôi phục thành công!";
-            return RedirectToAction("Table");
-        }
+      ViewData["Title"] = "Danh mục đơn vị tính";
+      webContext context = new webContext();
+      TempData["Menu"] = context.Menu.FirstOrDefault(menu => EF.Functions.Like(menu.TenMenu, "%Danh mục đơn vị tính%") && menu.Active == true).Id;
+      return View("TableDonViTinh");
     }
+
+    //Trả về view thêm đơn vị tính
+    public IActionResult ViewInsertDVT()
+    {
+      ViewData["Title"] = "Thêm đơn vị tính";
+      return View();
+    }
+    [HttpPost("/loadTableDVT")]
+    public IActionResult loadTableDVT(bool active)
+    {
+      webContext context = new webContext();
+      if (active)
+      {
+        ViewBag.DVT = context.Dvt.Where(x => x.Active == true).ToList();
+      }
+      else
+      {
+        ViewBag.DVT = context.Dvt.ToList();
+      }
+      return PartialView();
+    }
+
+    //Trả về view sửa đơn vị tính
+    [Route("/DonViTinh/ViewUpdateDVT/{id}")]
+    public IActionResult ViewUpdateDVT(int id)
+    {
+      ViewData["Title"] = "Sửa đơn vị tính";
+      webContext context = new webContext();
+      Dvt dvt = context.Dvt.Find(id);
+      return View(dvt);
+    }
+
+    //insert đơn vị tính
+    [HttpPost]
+    public IActionResult insertDonViTinh(Dvt dvt)
+    {
+      webContext context = new webContext();
+      dvt.Nvtao = 3;
+      dvt.Active = true;
+      dvt.NgayTao = DateTime.Now;
+      context.Dvt.Add(dvt);
+      context.SaveChanges();
+      TempData["ThongBao"] = "Thêm thành công!";
+      return RedirectToAction("Table");
+    }
+
+    //update đơn vị tính
+    [HttpPost]
+    public IActionResult updateDVt(Dvt dvt)
+    {
+      webContext context = new webContext();
+      Dvt dv = context.Dvt.Find(dvt.Id);
+
+      dv.Nvsua = 3;
+      dv.NgaySua = DateTime.Now;
+      dv.TenDvt = dvt.TenDvt;
+      dv.MaDvt = dvt.MaDvt;
+
+      context.Dvt.Update(dv);
+      context.SaveChanges();
+      TempData["ThongBao"] = "Sửa thành công!";
+      return RedirectToAction("Table");
+    }
+
+    //Xoá đơn vị tính
+    [Route("/DonViTinh/xoa/{id}")]
+    public IActionResult deleteDonViTinh(int id)
+    {
+      webContext context = new webContext();
+      Dvt dvt = context.Dvt.Find(id);
+      dvt.Active = false;
+
+      context.Dvt.Update(dvt);
+      context.SaveChanges();
+      return RedirectToAction("Table");
+    }
+    [Route("/DonViTinh/khoiphuc/{id}")]
+    public IActionResult khoiphucDVT(int id)
+    {
+      webContext context = new webContext();
+      Dvt dvt = context.Dvt.Find(id);
+      dvt.Active = true;
+
+      context.Dvt.Update(dvt);
+      context.SaveChanges();
+      TempData["ThongBao"] = "Khôi phục thành công!";
+      return RedirectToAction("Table");
+    }
+  }
 }
--- VatTuYTeDanhMuc/Controllers/LoginController.cs
+++ VatTuYTeDanhMuc/Controllers/LoginController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication;
+﻿//using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
@@ -10,101 +12,164 @@ using VatTuYTeDanhMuc.Models.Entities;
 
 namespace VatTuYTe.Controllers
 {
-    public class LoginController : Controller
+  public class LoginController : Controller
+  {
+    private readonly ILogger<LoginController> _logger;
+
+    public LoginController(ILogger<LoginController> logger)
     {
-      private readonly ILogger<LoginController> _logger;
+      _logger = logger;
+    }
 
-      public LoginController(ILogger<LoginController> logger)
-      {
-          _logger = logger;
-      }
-    
-      private webContext context = new webContext();
-      public IActionResult Login()
-      {
-          return View();
-      }
+    private webContext context = new webContext();
+    public IActionResult Login()
+    {
+      return View();
+    }
 
-      [Route("/Login")]
-      public IActionResult LogIn(string returnUrl)
-      {
-        TempData["returnUrl"] = returnUrl;
-        return View();
-      }
+    [Route("/Login")]
+    public IActionResult LogIn(string returnUrl)
+    {
+      TempData["returnUrl"] = returnUrl;
+      return View();
+    }
 
-      [HttpPost]
-      public ActionResult LogIn(TaiKhoan account, string returnUrl)
+    [Authorize(Roles = "NV")]
+    public IActionResult ViewSelector()
+    {
+      ViewBag.TaiKhoan = TempData["TaiKhoan"];
+      ViewBag.ChiNhanh = TempData["ChiNhanh"];
+      return View();
+    }
+
+
+
+
+
+    [HttpPost]
+    [Authorize(Roles = "NV")]
+    public IActionResult Selector(PhanQuyen pq)
+    {
+      webContext context = new webContext();
+
+      var identity = new ClaimsIdentity(User.Identity);
+      identity.AddClaim(new Claim("VaiTro", pq.Idvt.ToString()));
+      identity.AddClaim(new Claim("ChiNhanh", pq.Idcn.ToString()));
+
+      var claims = identity.Claims.ToList();
+
+      var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+      HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+      return RedirectToAction("index","Home");
+    }
+
+
+    [HttpPost("/loadQuyen")]
+    [Authorize(Roles = "NV")]
+    public IActionResult loadVaiTro(string tk, int idcn)
+    {
+      webContext context = new webContext();
+
+      int idtk = context.TaiKhoan.FirstOrDefault(k => k.Active == true && k.TenTaiKhoan == tk).Id;
+
+      if (idcn != 0)
       {
-        if (ModelState.IsValid)
+        var listvt = context.PhanQuyen.Where(a => a.Idcn == idcn && a.Idtk == idtk && a.Active == true).Select(j => j.Idvt).Distinct().ToList();
+        ViewBag.ListPQCN = listvt;
+      }
+      else
+      {
+        var listvt = context.PhanQuyen.Where(a => a.Idtk.Equals(idtk) && a.Active == true).Select(j => j.Idvt).Distinct().ToList();
+        ViewBag.ListPQCN = listvt;
+      }    
+      return PartialView();
+    }
+
+
+
+    [HttpPost]
+    public ActionResult LogIn(TaiKhoan account, string returnUrl)
+    {
+      if (ModelState.IsValid)
+      {
+        returnUrl = (string)TempData["returnUrl"];
+        string user = account.TenTaiKhoan;
+        string pass = account.MatKhau;
+        var acc = context.TaiKhoan.FirstOrDefault(s => s.TenTaiKhoan.Equals(user) && s.MatKhau.Equals(pass));
+        if (acc != null)
         {
-          returnUrl = (string)TempData["returnUrl"];
-          string user = account.TenTaiKhoan;
-          string pass = account.MatKhau;
-          var acc = context.TaiKhoan.FirstOrDefault(s => s.TenTaiKhoan.Equals(user) && s.MatKhau.Equals(pass));
-          if (acc != null)
-          {
-            PhanQuyen vaitro = context.PhanQuyen.FirstOrDefault(c => c.Idtk.Equals(acc.Id));
-            if (acc.Loai == true)
-            {           
-              NhanVien nv = context.NhanVien.FirstOrDefault(b => b.TenTaiKhoan.Equals(acc.TenTaiKhoan));                          
-              //int vt = vaitro.Id;
+          // PhanQuyen vaitro = context.PhanQuyen.FirstOrDefault(c => c.Idtk.Equals(acc.Id) && c.Active == true);
 
-              var claims = new List<Claim>
+          var listpq = context.PhanQuyen.Where(j => j.Idtk.Equals(acc.Id) && j.Active == true).Select(j => j.Id).Distinct().ToList();
+          TempData["TaiKhoan"] = user;
+          TempData["ChiNhanh"] = listpq;
+
+          if (acc.Loai == true)
+          {
+            NhanVien nv = context.NhanVien.FirstOrDefault(b => b.TenTaiKhoan.Equals(acc.TenTaiKhoan));
+
+            var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, account.TenTaiKhoan),
                     new Claim(ClaimTypes.Role, "NV"),
-                    new Claim("VaiTro", vaitro.Idvt.ToString()),
+                    //new Claim("User", nv.Idcn.ToString()),
                     new Claim(nv.Id.ToString(), nv.TenNv),
                 };
-              var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-              HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
 
-              if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                                && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-              {
-                return Redirect(returnUrl);
-              }
-              else
-              {
-                return RedirectToAction("Index", "Home");
-              }
+
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+              return Redirect(returnUrl);
             }
             else
             {
-              KhachHang kh = context.KhachHang.FirstOrDefault(s => s.TenTaiKhoan.Equals(acc.TenTaiKhoan));
-              var claims = new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name, account.TenTaiKhoan),
-                    new Claim(ClaimTypes.Role, "KH"),
-                    new Claim("VaiTro", vaitro.Idvt.ToString()),
-                    new Claim(kh.Id.ToString(), kh.TenKh),
-                };
-              var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-              HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-
-              if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
-                                && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
-              {
-                return Redirect(returnUrl);
-              }
-              else
-              {
-                return RedirectToAction("Index", "Home");
-              }
+              return RedirectToAction("ViewSelector");
+              //return RedirectToAction("Index", "Home");
             }
           }
           else
           {
-            ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không chính xác!");
+            KhachHang kh = context.KhachHang.FirstOrDefault(s => s.TenTaiKhoan.Equals(acc.TenTaiKhoan));
+            PhanQuyen vaitro = context.PhanQuyen.FirstOrDefault(c => c.Idtk.Equals(acc.Id) && c.Active == true);
+            var claims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, account.TenTaiKhoan),
+                    new Claim(ClaimTypes.Role, "KH"),                 
+                    new Claim(kh.Id.ToString(), kh.TenKh),
+                    new Claim("VaiTro", vaitro.Idvt.ToString()),
+                };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            if (Url.IsLocalUrl(returnUrl) && returnUrl.Length > 1 && returnUrl.StartsWith("/")
+                              && !returnUrl.StartsWith("//") && !returnUrl.StartsWith("/\\"))
+            {
+              return Redirect(returnUrl);
+            }
+            else
+            {
+              return RedirectToAction("Index", "Home");
+            }
           }
         }
-        return View();
+        else
+        {
+          ModelState.AddModelError("", "Tài khoản hoặc mật khẩu không chính xác!");
+        }
       }
-      [Authorize]
-      public ActionResult LogOut()
-      {
-        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-        return RedirectToAction("Login");
-      }
+      return View();
+    }
+    
+    
+    [Authorize]
+    public ActionResult LogOut()
+    {
+      HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+      return RedirectToAction("Login");
+    }
   }
 }
