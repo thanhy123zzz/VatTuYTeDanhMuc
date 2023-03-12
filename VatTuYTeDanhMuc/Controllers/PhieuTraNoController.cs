e59c0cbb46f4078e2f7185a218400044c7b38636
Bao Yen <nneyel104@gmail.com>
2023-03-13 03:00:15 +0700

    test1

---
+++ VatTuYTeDanhMuc/Controllers/PhieuTraNoController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VatTuYTeDanhMuc.Models.Entities;
+
+namespace VatTuYTeDanhMuc.Controllers
+{
+  public class PhieuTraNoController : Controller
+  {
+
+    private ICompositeViewEngine _viewEngine;
+    public PhieuTraNoController(ICompositeViewEngine viewEngine)
+    {
+      _viewEngine = viewEngine;
+    }
+
+    [Route("/PhieuTraNo")]
+    public IActionResult Index()
+    {
+      return View("PhieuTraNo");
+    }
+
+
+    [HttpPost("/getNganHangNCC")]
+    public IActionResult loadNHNCC(int idncc)
+    {
+      webContext context = new webContext();
+    //  List<NccNganHang> listnhncc = context.NccNganHang.Where(a => a.Idncc == idncc && a.Active == true).ToList();
+      var listnhncc = context.NccNganHang.Where(a => a.Idncc == idncc && a.Active == true).Include(x => x.IdnccNavigation).Include(x => x.IdnhNavigation).ToList();
+      //  ViewBag.ListNHNCC = listnhncc;
+      //get taikhoan(idtk)
+
+      int i = 0;
+      string options = null;// = "<option selected value = '" + hh.Iddvtchinh + "'>" + hh.IddvtchinhNavigation.TenDvt + "</option>";
+      foreach (NccNganHang d in listnhncc)
+      {
+        if (i == 0)
+        {
+          options = "<option selected value = '" + d.Idnh + "'>" + d.IdnhNavigation.TenNh + "</option>";
+        }
+        else 
+        { 
+          options += "<option value = '" + d.Idnh + "'>" + d.IdnhNavigation.TenNh + "</option>";
+        }
+        i++;
+      }
+
+      return Json(new
+      {
+        options = options
+      });
+
+      //return PartialView();
+    }
+
+
+
+
+
+
+  }
+}
---
+++ VatTuYTeDanhMuc/Models/PhieuTraNoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VatTuYTeDanhMuc.Models.Entities;
+
+namespace VatTuYTeDanhMuc.Models
+{
+  public class PhieuTraNoModel
+  {
+    public PhieuTraNo phieuTraNo { get; set; }
+
+    public NccNganHang nccNganHang { get; set; }
+
+    public List<ChiTietTraNo> chiTietTraNos { get; set; }
+  }
+}
